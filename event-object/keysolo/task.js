1fa6e22b65c7bd07e9f3ccb12e8c67cf271bee03
Andrey Vinokurov <kreketjot@gmail.com>
2021-05-07 10:04:58 +0300

    hw 1.3.2

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    // почему если вместо window поставить this.container, то работать не будет?
+    window.addEventListener('keydown', evt => {
+      if (evt.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
