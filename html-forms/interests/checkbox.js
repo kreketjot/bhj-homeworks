2b1a201d1462a0fd3f6066c36558eecca819b1d3
Andrey Vinokurov <kreketjot@gmail.com>
2021-05-14 17:56:40 +0300

    hw 2.2.2 updated

--- html-forms/interests/checkbox.js
+++ html-forms/interests/checkbox.js
@@ -1,74 +1,72 @@
 'use strict'
 
 class CheckBox {
-  _parent;
-  _element;
-  _children;
+  #_parent;
+  #_element;
+  #_children;
 
   constructor(el, par = null) {
-    this._parent = par;
-    this._element = el;
-    const chlds = el.closest('li.interest').querySelector('ul.interests');
+    this.#_parent = par;
+    this.#_element = el.querySelector('.interest__check');
+    this.#_children = [];
+    const chlds = el.querySelector('ul.interests');
     if (chlds) {
-      const chbs = [...chlds.children].filter( el => el.matches('li.interest') ).
-        map( li => li.querySelector('.interest__check') );
-      setTimeout( () => this._children = chbs.map( chb => new CheckBox(chb, this)) );
-    } else {
-      this._children = [];
+      const chbs = [...chlds.children].filter( el => el.matches('li.interest') );
+      setTimeout( () => this.#_children = chbs.map( li => new CheckBox(li, this)) );
     }
 
-    this._element.addEventListener('click', evt => {
-      if (this._element.checked) {
-        this.enableChildren();
-        this.startEnablingParent();
+    this.#_element.addEventListener('click', evt => {
+      if (this.#_element.checked) {
+        this.#enableChildren();
+        this.#startEnablingParent();
       } else {
-        this.disableChildren();
-        this.startDisablingParent();
+        this.#disableChildren();
+        this.#startDisablingParent();
       }
     });
   }
 
-  enableChildren() {
-    this._element.checked = true;
-    this._element.indeterminate = false;
-    this._children.forEach( child => child.enableChildren() );
+  #enableChildren() {
+    this.#_element.checked = true;
+    this.#_element.indeterminate = false;
+    this.#_children.forEach( child => child.#enableChildren() );
   }
 
-  disableChildren() {
-    this._element.checked = false;
-    this._element.indeterminate = false;
-    this._children.forEach( child => child.disableChildren() );
+  #disableChildren() {
+    this.#_element.checked = false;
+    this.#_element.indeterminate = false;
+    this.#_children.forEach( child => child.#disableChildren() );
   }
 
-  startEnablingParent() {
-    this._parent?.enableParent();
+  #startEnablingParent() {
+    this.#_parent && this.#_parent.#enableParent();
   }
 
-  startDisablingParent() {
-    this._parent?.disableParent();
+  #startDisablingParent() {
+    this.#_parent && this.#_parent.#disableParent();
   }
 
-  enableParent() {
-    const notFull = this._children.find( chb => !chb._element.checked );
-    if (notFull) {
-      this._element.indeterminate = true;
-    } else {
-      this._element.checked = true;
-      this._element.indeterminate = false;
-    }
-    this.startEnablingParent();
-  }
-
-  disableParent() {
-    const has = this._children.find( chb => 
-      chb._element.checked || chb._element.indeterminate );
+  #enableParent() {
+    const has = this.#_children.some( chb => !chb.#_element.checked );
     if (has) {
-      this._element.checked = false;
-      this._element.indeterminate = true;
+      this.#_element.indeterminate = true;
     } else {
-      this._element.checked = false;
-      this._element.indeterminate = false;
+      this.#_element.checked = true;
+      this.#_element.indeterminate = false;
     }
-    this.startDisablingParent();
+    this.#startEnablingParent();
+  }
+
+  #disableParent() {
+    const has = this.#_children.some( chb => 
+      chb.#_element.checked || chb.#_element.indeterminate );
+    if (has) {
+      this.#_element.checked = false;
+      this.#_element.indeterminate = true;
+    } else {
+      this.#_element.checked = false;
+      this.#_element.indeterminate = false;
+    }
+    this.#startDisablingParent();
   }
 }
--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,5 +2,5 @@
 
 {
   const interests = [...document.querySelectorAll('.interests_main > ul > li.interest')]
-  const checkboxes = interests.map( el => new CheckBox( el.querySelector('.interest__check') ) );
+  const checkboxes = interests.map( el => new CheckBox(el) );
 }
